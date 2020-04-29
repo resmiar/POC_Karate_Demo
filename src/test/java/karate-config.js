9181ecace039ed5622ec2784ed8e5412b3bfd91f
shinith <shinithramapuram@gmail.com>
2020-04-29 10:42:23 +0530

    fail retry config added

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -16,5 +16,6 @@ function fn() {
   // don't waste time waiting for a connection or if servers don't respond within 5 seconds
   karate.configure('connectTimeout', 5000);
   karate.configure('readTimeout', 5000);
+  // karate.configure('retry', { count: 6, interval: 1000 })
   return config;
 }
